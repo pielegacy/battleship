07fbd647877f47f7918319874b1c7aae03a26a17
Alex Billson <strikingmongoose@hotmail.com>
2017-04-07 12:52:04 +1000

    Updated game logic

--- Source-CSharp/battleships/src/GameLogic.cs
+++ Source-CSharp/battleships/src/GameLogic.cs
@@ -1,10 +1,10 @@
-
-
 using System;
 using System.Collections;
 using System.Collections.Generic;
 //using System.Data;
 using System.Diagnostics;
+using SwinGameSDK;
+
 namespace Battleships
 {
 static class GameLogic
@@ -15,20 +15,20 @@ static class GameLogic
 		SwinGame.OpenGraphicsWindow("Battle Ships", 800, 600);
 
 		//Load Resources
-		LoadResources();
+		GameResources.LoadResources();
 
-		SwinGame.PlayMusic(GameMusic("Background"));
+		SwinGame.PlayMusic(GameResources.GameMusic("Background"));
 
 		//Game Loop
 		do {
-			HandleUserInput();
-			DrawScreen();
-		} while (!(SwinGame.WindowCloseRequested() == true | CurrentState == GameState.Quitting));
+			GameController.HandleUserInput();
+			GameController.DrawScreen();
+		} while (!(SwinGame.WindowCloseRequested() == true | GameController.CurrentState == GameState.Quitting));
 
 		SwinGame.StopMusic();
 
 		//Free Resources and Close Audio, to end the program.
-		FreeResources();
+		GameResources.FreeResources();
 	}
 }
 
