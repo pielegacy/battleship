d5b859e19f2260e1e55982ccc8f185d492aed3d3
Alex Billson <strikingmongoose@hotmail.com>
2017-04-07 10:45:42 +1000

    Added event handler change

--- Source-CSharp/battleships/src/Model/SeaGrid.cs
+++ Source-CSharp/battleships/src/Model/SeaGrid.cs
@@ -26,7 +26,7 @@ public class SeaGrid : ISeaGrid
 	/// <summary>
 	/// The sea grid has changed and should be redrawn.
 	/// </summary>
-	public event EventHandler ISeaGrid.Changed;
+	public event EventHandler Changed;
 
 	/// <summary>
 	/// The width of the sea grid.
