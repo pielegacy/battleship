a924df00574cf99dfa48f0f66191e724594edd14
pielegacy <alex_billson@outlook.com>
2017-04-09 19:36:06 +1000

    SeaGrid modified

--- Source-CSharp/battleships/src/Model/SeaGridAdapter.cs
+++ Source-CSharp/battleships/src/Model/SeaGridAdapter.cs
@@ -46,9 +46,9 @@ public class SeaGridAdapter : ISeaGrid
 	/// <param name="x">tile x coordinate</param>
 	/// <param name="y">tile y coordinate</param>
 	/// <returns>a tile, either what it actually is, or if it was a ship then return a sea tile</returns>
-	public TileView Item {
+	public TileView this[int x, int y] {
 		get {
-			TileView result = _MyGrid.Item(x, y);
+			TileView result = _MyGrid[x, y];
 
 			if (result == TileView.Ship) {
 				return TileView.Sea;
